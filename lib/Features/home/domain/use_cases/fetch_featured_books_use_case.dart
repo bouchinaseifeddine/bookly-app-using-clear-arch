efbeddb556169024ad3ecdfaad10ef144d934fac
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-04-01 12:32:27 +0100

    create generic use case

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,17 +1,19 @@
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/use_cases.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, void> {
   final HomeRepo homeRepo;
 
   FetchFeaturedBooksUseCase({required this.homeRepo});
 
-  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+  @override
+  Future<Either<Failure, List<BookEntity>>> call([void param]) async {
     // in this case we dont need the use case
     // but when we need to add extra code like (request permission / check for something ..)
     // we can add it here
-    return homeRepo.fetchFeaturedBooks();
+    return await homeRepo.fetchFeaturedBooks();
   }
 }
---
+++ lib/core/use_cases/use_cases.dart
@@ -0,0 +1,6 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class UseCase<Type, Param> {
+  Future<Either<Failure, Type>> call([Param param]);
+}
