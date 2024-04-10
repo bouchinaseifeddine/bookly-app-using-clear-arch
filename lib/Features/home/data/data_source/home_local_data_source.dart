c077582a67b23634891cd3fa8a1855f25a51bcaf
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-04-10 22:02:59 +0100

    create home local data source

---
+++ lib/Features/home/data/data_source/home_local_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
