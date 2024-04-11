75bd92169de9a8d6c9ec0ed1561e85d4edeaf16e
bouchinaseifeddine <bouchinaseifeddine@gmail.com>
2024-04-11 11:53:20 +0100

    cache featured books

--- lib/Features/home/data/data_source/home_remote_data_source.dart
+++ lib/Features/home/data/data_source/home_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/core/utils/functions/save_books.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
@@ -18,6 +20,7 @@ class HomeRemoteDataSourceImpl implements HomeRemoteDataSource {
 
     List<BookEntity> books = getBooksList(data);
 
+    saveBooksData(books, kFeaturedBox);
     return books;
   }
 
---
+++ lib/core/utils/functions/save_books.dart
@@ -0,0 +1,7 @@
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
+
+void saveBooksData(List<BookEntity> books, String boxName) {
+  var box = Hive.box(boxName);
+  box.addAll(books);
+}
